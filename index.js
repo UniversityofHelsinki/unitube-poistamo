9fa26188b1bba7b566109072e13f3e5f8ee26b37
vmheikki <veli-matti.heikkila@helsinki.fi>
2021-11-30 15:10:39 +0200

    added helmet.js , compression.js and body-parser middlewares
    added environment variables for OpenShift

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+'use strict';
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const compression = require('compression');
 const database = require("./services/database");
 const fs = require("fs");
 const path = require("path");
+
+const ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+app.use(compression());
+app.use(helmet());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 // CREATE TABLES
 const createTables = fs.readFileSync(path.resolve(__dirname, "./sql/createTables.sql"), "utf8");
@@ -15,6 +28,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port, ipaddress, () => {
+    console.log( 'Listening on ' + ipaddress + ', port ' + port );
 });
