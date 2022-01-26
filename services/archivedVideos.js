b7e5b7cc2a26a26680b4f366e858b4892128029e
Henriikka Palva <henriikka.palva@helsinki.fi>
2022-01-26 12:13:09 +0200

    Cron time change and refactor

--- services/archivedVideos.js
+++ services/archivedVideos.js
@@ -10,7 +10,7 @@ const archiveVideos = async(archivedVideos) => {
         console.log("video id to be archived: ", videoId);
         try {
             const eventResponse = await apiService.getEvent(videoId);
-            if (eventResponse.status != '200') {
+            if (eventResponse.status !== 200) {
                 await databaseService.insertIntoVideoLogs(eventResponse.status, `error no video found for this id`, videoId );
                 // something went wrong continue to next video
                 continue;
@@ -18,7 +18,7 @@ const archiveVideos = async(archivedVideos) => {
             const archivedSeriesId = process.env.POISTAMO_OPENCAST_ARCHIVED_SERIES;
             // call api service to move video to archived series
             const archiveResponse = await apiService.moveVideoToArchivedSeries(eventResponse.data, archivedSeriesId);
-            if (archiveResponse.status != '200') {
+            if (archiveResponse.status !== 200) {
                 // insert into video_logs table for error in operation
                 await databaseService.insertIntoVideoLogs(archiveResponse.status, `error moving from series : ${eventResponse.data.is_part_of} to archived series : ${archivedSeriesId} ${archiveResponse.statusText}`, videoId );
                 // something went wrong continue to next video
