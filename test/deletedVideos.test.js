63ed9855c5938c669ddd7ab590f9868882e76b74
Vellu <veli-matti.heikkila@helsinki.fi>
2024-05-28 13:29:51 +0300

    remove thumbnail image from thumbnails table during video deletion

--- test/deletedVideos.test.js
+++ test/deletedVideos.test.js
@@ -34,6 +34,12 @@ beforeAll(async () => {
 beforeEach(async () => {
     await client.query('CREATE TEMPORARY TABLE video_logs(video_log_id SERIAL NOT NULL, status_code VARCHAR(255) NOT NULL, oc_messages VARCHAR(255) NOT NULL, video_id VARCHAR(255) NOT NULL, video_name VARCHAR(255), original_series_id VARCHAR(255), original_series_name VARCHAR(255), archived_series_id varchar(255), PRIMARY KEY(video_log_id))');
     await client.query('CREATE TEMPORARY TABLE videos(video_id VARCHAR(255) NOT NULL, archived_date date, actual_archived_date date, deletion_date date, video_creation_date date, error_date date, PRIMARY KEY(video_id))');
+    await client.query('CREATE TEMPORARY TABLE THUMBNAILS(video_id VARCHAR(255) NOT NULL,\n' +
+        '    thumbnail BYTEA,\n' +
+        '    PRIMARY KEY(video_id),\n' +
+        '    CONSTRAINT fk_video_id\n' +
+        '        FOREIGN KEY(video_id)\n' +
+        '            REFERENCES videos(video_id))')
     await wait(100);
     await client.query('INSERT INTO videos (video_id, archived_date, video_creation_date) VALUES (\'e8a86433-0245-44b8-b0d7-69f6578bac6f\', \'2018-01-01\'::date, \'2008-01-01\'::date)');
     await timer.getTimer.mockResolvedValue(0);
@@ -41,6 +47,7 @@ beforeEach(async () => {
 
 afterEach(async () => {
     await wait(100);
+    await client.query('DROP TABLE IF EXISTS pg_temp.thumbnails');
     await client.query('DROP TABLE IF EXISTS pg_temp.videos');
     await client.query('DROP TABLE IF EXISTS pg_temp.video_logs');
 });
