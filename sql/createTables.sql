efc05d6c0457b4b91a872c99e7e8f6fa9d0c4410
Vellu <veli-matti.heikkila@helsinki.fi>
2026-08-24 09:14:12 +0300

    faculties_departments table creation

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -275,10 +275,14 @@ CREATE TABLE IF NOT EXISTS mediaitem_transcriptions (
         REFERENCES mediaItem(id)
 );
 
+
 CREATE TABLE IF NOT EXISTS faculties_departments (
                                                      id SERIAL PRIMARY KEY,
                                                      unique_id INTEGER NOT NULL,
-                                                     unit_type VARCHAR(255) NOT NULL
+                                                     unit_type VARCHAR(255) NOT NULL,
+    name_fi VARCHAR(255) NOT NULL,
+    name_sv VARCHAR(255) NOT NULL,
+    name_en VARCHAR(255) NOT NULL
     );
 
 
